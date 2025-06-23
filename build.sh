4e7cf8fa98647afaa39a12584888abc5d3958609
saler1942 <saler1942@gmail.com>
2025-06-23 23:20:16 +0500

    пред финальный коммит

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+set -e
 
 pip install -r requirements.txt
-
-cd tiketon
 mkdir -p staticfiles
 python manage.py collectstatic --no-input --clear
 python manage.py migrate 
