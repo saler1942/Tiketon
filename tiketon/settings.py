595b80c6b61c600e83477f428ba8670f9254e7f2
saler1942 <saler1942@gmail.com>
2025-08-23 00:34:44 +0500

    fix

--- tiketon/settings.py
+++ tiketon/settings.py
@@ -100,13 +100,6 @@ DATABASES = {
 }
 
 # Use PostgreSQL in production
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
 
 
 # Password validation
