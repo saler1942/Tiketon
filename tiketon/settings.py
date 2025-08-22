48a1ea37163527557ba8d576ea891d8848e3793e
saler1942 <saler1942@gmail.com>
2025-08-23 00:46:14 +0500

    fix ПОСЛЕДНИЙ

--- tiketon/settings.py
+++ tiketon/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'tiketon.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "/var/data/db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
